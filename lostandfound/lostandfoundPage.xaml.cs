667d8c8c0f0d0090f0f2daa592eb97e986546898
Abdul Muhaymin <mampary@hotmail.com>
2017-04-28 20:48:35 +0530

    Initial Setup

---
+++ lostandfound/lostandfoundPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace lostandfound
+{
+	public partial class lostandfoundPage : ContentPage
+	{
+		public lostandfoundPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
